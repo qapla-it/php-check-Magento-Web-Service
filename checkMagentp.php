4f62f82fb64d4840be87a4e9d2fbfa6df0bda7cc
luca <pippero.qapla.13>
2017-12-08 17:21:30 +0100

    initial commit

--- checkMagentp.php
+++ checkMagentp.php
@@ -1,6 +1,11 @@
 <?php
 $apiKey = '{Qapla\' API Key}';
+
 $magentoURL = '{Mangeto url}'.'/index.php/api/v2_soap/?wsdl';
+
+$dateFrom = date('Y-m-d').' 00:00:00';
+
+echo '<h4>Connecting to '.$magentoURL.'</h4>';
 
 /** Soap connection */
 try{
@@ -21,11 +26,13 @@ catch (Exception $e){
 echo '&#10003; Login OK<br/>';
 
 /** salesOrderList */
+echo '<h4>Getting orders starting from '.$dateFrom.'</h4>';
+
 $complexFilter = [
     'complex_filter' => [
-        ['key' => 'updated_at', 'value' => ['key' => 'from','value' => date('Y-m-d').' 00:00:00'],],
+        ['key' => 'updated_at', 'value' => ['key' => 'from','value' => $dateFrom],],
     ]];
-    
+
 try{
     $result = $proxy->salesOrderList($sessionId, $complexFilter);
 }
@@ -33,12 +40,10 @@ catch(Exception $e){
     exit('Cannot read order list.'.$e->__toString());
 }
 
-echo '&#10003; Order list OK<br/>';
-
 if(!count($result)):
-    echo '&#10003; No orders found today';
+    echo '<h4>No orders found today</h4>';
 else:
-    echo '&#10003; '.count($result).' orders found<table class="mTop10">';
+    echo '<h4>'.count($result).' orders found</h4><table>';
 
     $rows = 0;
 
@@ -49,7 +54,7 @@ else:
         }
         catch (Exception $e){
             exit('Cannot get the order.'.$e->__toString());
-		  }
+        }
 
         echo '<tr><td width="50">'.sprintf("%03s",++$rows).'</td><td width="250">'.$order->increment_id.'</td><td width="200">'.$order->updated_at.'</td><td width="200">'.$order->status.'</td></tr>';
 
@@ -62,11 +67,13 @@ else:
     echo '<h4>Getting order: '.$order->increment_id.', id: '.$orderID.'</h4>';
 
     /** salesOrderList */
-    $filter = array('filter' => array(array('key' => 'order_id', 'value' => $orderID)));
+    $filter = ['filter' => [['key' => 'order_id', 'value' => $orderID]]];
     try{
         $order = $proxy->salesOrderList($sessionId, $filter);
     }
     catch(Exception $e) {
         exit('Cannot get the order.'.$e->__toString());
     }
+
+    var_dump($order);
 endif;
